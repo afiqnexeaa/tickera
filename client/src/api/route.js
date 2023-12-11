be23e54ba8023c635243a4f67f67ae482598ff42
afiqnexea <afiqnrzm@hotmail.com>
2023-12-11 17:52:19 +0800

    client

---
+++ client/src/api/route.js
@@ -0,0 +1,23 @@
+import React from "react";
+import axios from "axios";
+import { notifications } from "@mantine/notifications";
+
+const instance = axios.create({
+  baseURL: "http://localhost:3011",
+});
+
+export const createAttendees = async (values) => {
+  try {
+    const res = await instance.post("/attendees/createAttendees", values);
+    return notifications.show({
+      title: res?.data?.message,
+      message: "Hey there, your code is awesome! 🤥",
+    });
+  } catch (error) {
+    return notifications.show({
+      color: "red",
+      title: error?.response?.data?.message,
+      message: "Hey there, your code is awesome! 🤥",
+    });
+  }
+};
